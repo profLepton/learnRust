83a6894ce2abab59b86e3a99fd0a698bfd9d54d6
Akshay Kolli <proflepton@gmail.com>
2023-01-13 09:16:57 -0500

    Cses

---
+++ cses/Introductory Problems/weird_algorithm.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+fn main() {
+    let mut input_text = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("Failed to read from stdin!");
+
+    let mut n = input_text.trim().parse::<u32>().expect("Not a u32!");
+
+    //println!("{}", n);
+
+    while n != 1 {
+        print!("{} ", n);
+
+        match n % 2 {
+            0 => n = n / 2,
+            1 => n = n * 3 + 1,
+            _ => unreachable!(),
+        }
+    }
+
+    print!("1");
+}
