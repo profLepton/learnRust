5c17eccd83fb96c00e899c7af5a3d913460abaed
Akshay Kolli <proflepton@gmail.com>
2023-01-15 20:05:40 -0500

    repetitions

--- cses/Introductory Problems/repetitions.rs
+++ cses/Introductory Problems/repetitions.rs
@@ -4,6 +4,21 @@ fn main() {
 
     std::io::stdin().read_line(&mut input_string).expect("Failed to read from stdin");
 
+    let mut c = 'A';
+    let mut ans = 1;
+    let mut counter = 0;
 
+    for char in input_string.chars() {
+        if c == char {
+            counter += 1;
+            ans = std::cmp::max(ans, counter);
+        }
+        else {
+            c = char;
+            counter = 1;
+        }
+    }
+
+    println!("{}", ans);
     
 }
